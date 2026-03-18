3aecac7c4fa68d55235299e56c3eda8bf4f8b9d0
cschorn <conradoschorn@outlook.com>
2026-03-18 18:41:41 -0300

    EstruturasCondicionais adicionado

---
+++ src/estruturascondicionais/EstruturasCondicionais.java
@@ -0,0 +1,39 @@
+package estruturascondicionais;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class EstruturasCondicionais {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("5Digite o primeiro número: ");
+        int primNumero = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int secNumero = scanner.nextInt();
+
+        scanner.close();
+
+        limparTela();
+
+        System.out.println("Você escolheu os números: " + primNumero + " e " + secNumero);
+
+        if(primNumero == secNumero){
+            System.out.println("Os números são iguais!");
+
+        } else if (primNumero > secNumero){
+            System.out.println("O número " + primNumero +" é maior!");
+
+        } else {
+            System.out.println("O numero " + secNumero + " é maior!");
+        }
+    }
+
+    public static void limparTela() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+}
---
+++ src/manipulacaostring/ManipulacaoString.java
@@ -0,0 +1,13 @@
+package manipulacaostring;
+
+public class ManipulacaoString {
+    public static  void main(String[] args){
+
+        String numero = "5, 9, 7";
+        int tipoInt = Integer.parseInt("7");
+
+        tipoInt += 10;
+
+        System.out.println(tipoInt);
+    }
+}
